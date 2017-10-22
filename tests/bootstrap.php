973e0b4690540590c27dad89f58c12a17a60b8b1
zed <zedsmail@163.com>
2017-10-22 16:24:30 +0800

    修改配置

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zed
+ * Date: 17-10-20
+ * Time: 下午4:12
+ */
+
+require_once 'vendor/autoload.php';
+
+$BOS_TEST_CONFIG = [
+    'credentials' => array(
+        'accessKeyId' => '',
+        'secretAccessKey' => '',
+        'sessionToken' => ''
+    ),
+    'endpoint' => 'http://gz.bcebos.com',
+    'stsEndpoint' => '',
+];
